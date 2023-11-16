4a8f0770755781f12e98a6690de03102e599e6bb
Mike Straw <mike.straw@automattic.com>
2023-11-16 11:15:48 -0500

    Remove JS and CSS related code

--- assets/js/src/editor.js
+++
@@ -1,9 +0,0 @@
-import { createHooks } from '@wordpress/hooks';
-import domReady from '@wordpress/dom-ready';
-
-window.wpcomsp_51_backfill = window.wpcomsp_51_backfill || {};
-window.wpcomsp_51_backfill.hooks = createHooks();
-
-domReady( () => {
-	window.wpcomsp_51_backfill.hooks.doAction( 'editor.ready' );
-} );
